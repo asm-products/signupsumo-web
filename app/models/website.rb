cb2ef19c62000f7164a30a77fe2530bf00bca458
Vinoth <a.vinothk@outlook.com>
2015-01-21 20:14:57 +0530

    move validations to model

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,9 +1,22 @@
 class Website < ActiveRecord::Base
+  include WebsitesHelper
 
   belongs_to :user
   has_many :registers, dependent: :delete_all
   has_many :profiles, :through => :registers
 
   validates :user_id, :name, :host, :secret_token, :presence => true
+
+  before_validation :format_url, :generate_token
+
+  private
+
+    def format_url
+      self.host = check_url(self.host)
+    end
+
+    def generate_token
+      self[:secret_token] = SecureRandom.hex
+    end
   
 end
