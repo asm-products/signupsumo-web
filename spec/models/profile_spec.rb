f7eee44731ba064fde2f141ed2dbd50ac0de79b3
Vinoth <a.vinothk@outlook.com>
2015-01-22 21:44:46 +0530

    add profile and register specs. add validations to register

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Profile, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validation' do
+    it 'should not create with empty email' do
+      profile = Profile.create(:data => 'strangepwd')
+      expect(profile.save).to be_falsey
+    end
+  end
+
+  describe 'relationships' do
+    it 'should not delete registers when profile is deleted' do
+      profile = Fabricate(:profile)
+      register = profile.registers.create!(:website => Fabricate(:website))
+      expect {
+          Profile.destroy(profile)
+        }.to_not change(Register, :count)
+    end
+  end
+
 end
--- spec/models/register_spec.rb
+++ spec/models/register_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Register, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validation' do
+    it 'should not create without website' do
+      register = Register.create(:profile => Fabricate(:profile))
+      expect(register.save).to be_falsey
+    end
+
+    it 'should not create without profile' do
+      register = Register.create(:website => Fabricate(:website))
+      expect(register.save).to be_falsey
+    end
+  end
+
+
 end
