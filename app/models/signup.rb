403da04d31952336dfb02f1a6ca9e6780dcbcfdc
vanstee <patrick@vanstee.me>
2015-01-30 17:21:43 -0500

    Include data in the email

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -7,4 +7,54 @@ class Signup < ActiveRecord::Base
 
   validates :user_id,
     presence: true
+
+  def name
+    person.fetch(:name, {}).
+      fetch[:full_name]
+  end
+
+  def details
+    title || bio
+  end
+
+  def title
+    return unless employment &&
+      employment[:name] &&
+      employment[:title]
+
+    "#{employment[:title]} at #{employement[:name]}"
+  end
+
+  def bio
+    person[:bio]
+  end
+
+  def location
+    person[:location]
+  end
+
+  def followers
+    person.fetch(:twitter, {}).
+      fetch[:followers]
+  end
+
+  def employment
+    person.fetch(:employment, {})
+  end
+
+  def person
+    data.fetch(:person, {})
+  end
+
+  def company
+    data.fetch(:company, {})
+  end
+
+  def score
+    data.fetch(:score, {})
+  end
+
+  def data
+    super.symbolize_keys
+  end
 end
