fed220a7196d4cade9709ed13454ced957f4b0ef
vanstee <patrick@vanstee.me>
2015-01-30 17:32:00 -0500

    Remove deprecation warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,10 +10,10 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  config.serve_static_files = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
