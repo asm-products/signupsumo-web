1ff3f7dae64c8aa64322f64b308015e2ecb90245
vanstee <patrick@vanstee.me>
2015-01-30 17:37:57 -0500

    Fix bug

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -4,7 +4,7 @@ class SignupsController < ApplicationController
 
     render json: { response: 'Bad request' } and return unless user
 
-    ChecksInfluence.perform_later(user, params[:email])
+    ChecksInfluenceJob.perform_later(user, params[:email])
 
     render json: { response: 'Submitted successfully' }
   end
