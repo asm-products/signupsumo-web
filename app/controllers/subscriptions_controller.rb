5c9dac8bc1cbc053fd687cb044e70b95846d2a62
Kenrick Beckett <kenrick.beckett@gmail.com>
2015-02-03 16:13:45 -0500

    Add stripe webhook to subscriptions controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,6 @@
 class SubscriptionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:webhook]
+  protect_from_forgery except: :webhook
 
   def show
     @subscription = current_user.subscription || Subscription.new
@@ -17,6 +18,23 @@ class SubscriptionsController < ApplicationController
     else
       render 'show'
     end
+  end
+
+  def webhook
+    event_json = JSON.parse(request.body.read)
+    event_type = event_json["type"]
+
+    if event_type == "invoice.payment_succeeded"
+      data = event_json["data"]["object"]
+      customer_id = data["customer"]
+      event_subscription = data["lines"]["subscriptions"].first
+      end_period = Time.at(event_subscription["period"]["end"])
+
+      subscription = Subscription.where("customer->>'id' = ?", customer_id).first
+      subscription.update_attributes(active_until: end_period) if subscription
+    end
+
+    render nothing: true, status: 200
   end
 
   def subscription_create_params
