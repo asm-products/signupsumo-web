363c9138ad30d780d744710d9e1d472cbfd44f8d
vanstee <patrick@vanstee.me>
2015-02-03 10:45:46 -0500

    Reuse constant for plan id

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
-unless Stripe::Plan.retrieve('100_monthly')
+unless Stripe::Plan.retrieve(Subscription::PLAN)
   Stripe::Plan.create(
     amount: 900,
     interval: 'monthly',
     name: '100 Monthly',
     currency: 'usd',
-    id: '100_monthly'
+    id: Subscription::PLAN
   )
 end
